74c5ca0316f4dba2ba81fba54c02fb282fba55a6
Simone <sdiricco.91@gmail.com>
2022-11-24 22:46:52 +0100

    add cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // In src/index.js
 const express = require("express");
 const bodyParser = require("body-parser");
+var cors = require('cors')
 const cityRouterV1 = require("./v1/routes/cityRoutes");
 
 const { swagger } = require("./v1/swagger");
@@ -8,6 +9,7 @@ const { swagger } = require("./v1/swagger");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors())
 app.use(bodyParser.json());
 app.use("/api/v1/cities", cityRouterV1);
 
